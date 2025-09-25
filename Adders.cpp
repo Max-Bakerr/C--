b92c1e83eb1f3285447d580048f12b695a8a7216
Max-Bakerr <maxpbaker@icloud.com>
2025-09-25 08:35:45 +0000

    commit 1

---
+++ Adders.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+int halfadd(int A, int B){
+    int Sum;
+    int Carry;
+    if(A =! B){
+        Sum = 1;
+    }
+    else(){
+        Sum = 0;
+    }
+
+    if(A = 1 && B = 1){
+        Carry = 1;
+    }
+
+    return(Sum, Carry);
+}
+
+int fulladd(A,B,C){
+    int Sum;
+    int Carry;
+    if((A != B) != C){
+        Sum = 1;
+    }
+    else(){
+        Sum = 0;
+    }
+
+    if((C && (A != B)) = 1 || (A & B) = 1){
+        Carry = 1;
+    }
+    else(){
+        Carry = 0;
+    }
+
+    return(Sum, Carry);
+}
+
+
+
+int main(){
+    std::cout << fulladd(1,1,0);
+
+}
