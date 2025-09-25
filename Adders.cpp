a83492a6de3779d42cbed3578d2ea054ac9ad57e
Max-Bakerr <maxpbaker@icloud.com>
2025-09-25 08:40:42 +0000

    commit 3

--- Adders.cpp
+++ Adders.cpp
@@ -6,7 +6,7 @@ int halfadd(int A, int B){
     if(A =! B){
         Sum = 1;
     }
-    else(){
+    else{
         Sum = 0;
     }
 
@@ -23,14 +23,14 @@ int fulladd(int A, int B, int C){
     if((A != B) != C){
         Sum = 1;
     }
-    else(){
+    else{
         Sum = 0;
     }
 
     if((C && (A != B)) = 1 || (A & B) = 1){
         Carry = 1;
     }
-    else(){
+    else{
         Carry = 0;
     }
 
